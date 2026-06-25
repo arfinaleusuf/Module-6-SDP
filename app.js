c9eb42068269bd2da18ef257946720e911e27405
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-26 00:13:54 +0600

    refactor: comment out unused variables and functions

--- app.js
+++ app.js
@@ -3,13 +3,23 @@
 // Tamplet litarels & Spread Operators
 
 
-const countryName = "bangladesh";
-const country = `my country name is ${countryName}`;
-// console.log(country);
+// const countryName = "bangladesh";
+// const country = `my country name is ${countryName}`;
+// // console.log(country);
 
-const numbers = [1,2,3,4,5,6,7,8,9];
-const numbers2 = [11,12,13,14,15,16,17,18,19];
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// const numbers2 = [11,12,13,14,15,16,17,18,19];
 
-const max = Math.max(...numbers);
-console.log(...numbers, ...numbers2);
-console.log(max);
+// const max = Math.max(...numbers);
+// console.log(...numbers, ...numbers2);
+// console.log(max);
+
+const person = {
+    name: "test",
+    age: 10,
+    friends: ["karim", "rahim", 'jobbar'],
+};
+
+const {age ,friends} = person;
+
+console.log(friends);
