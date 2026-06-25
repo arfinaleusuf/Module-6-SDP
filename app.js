3a004c7a6cd4103228d587a1b3084b857665df7f
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-25 22:45:04 +0600

    max value with sparede function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // let and const ES6
+
+// Tamplet litarels & Spread Operators
+
+
+const countryName = "bangladesh";
+const country = `my country name is ${countryName}`;
+// console.log(country);
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+const numbers2 = [11,12,13,14,15,16,17,18,19];
+
+const max = Math.max(...numbers);
+console.log(...numbers, ...numbers2);
+console.log(max);
