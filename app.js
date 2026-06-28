bdcd0c980b5ce9642ab8e111f896f56a578016fe
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-28 17:19:49 +0600

    problem 1

--- app.js
+++ app.js
@@ -68,7 +68,27 @@ const products = [
 // const sum = products.map(product => product.id*2);
 // console.log(sum);
 
-const result = products.forEach(product=> {
-    console.log(product.id);
-});
+// const result = products.forEach(product=> {
+//     console.log(product.id);
+// });
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const oddEven = (array) => {
+    let evenNumber = [0];
+
+    for(let i = 0; i< array.length; i++)
+    {
+        const element = array[i];
+
+        if (element%2 == 0)
+        {
+            evenNumber.push(element)
+        }
+    }
+    return evenNumber;
+}
+
+const result = oddEven(numbers)
+
 console.log(result);
